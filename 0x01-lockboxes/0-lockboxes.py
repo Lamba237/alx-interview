6e9a696b8db97ae5dead067f45ed334e7e03a621
Lamba237 <simontanyi2004@gmail.com>
2024-08-01 22:26:29 +0000

    This python function utilizes list and start to check if boxes are open

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # first create a set to keep track of which boxes have been open
+    n = len(boxes)
+    opened = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        if box not in opened:
+            opened.add(box)
+            for key in boxes[box]:
+                if key < n and key not in opened:
+                    stack.append(key)
+
+    return len(opened) == n
