3174936adf4e271b79cd11135c27891d20326d6d
Lamba237 <simontanyi2004@gmail.com>
2024-10-10 22:33:54 -0700

    A method that determines if all the boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,13 +10,17 @@ def canUnlockAll(boxes):
       Return: a boolean value
     """
 
+    n = len(boxes)
+
     opened_boxes = set()
-    keys = [0]
+    Stack = [0]
 
-    while keys:
-        key = keys.pop()
-        if key not in opened_boxes:
-            opened_boxes.add(key)
-            keys.extend(boxes[key])
+    while Stack:
+        box = Stack.pop()
+        if box not in opened_boxes:
+            opened_boxes.add(box)
+            for key in boxes[box]:
+                if key < n and key not in opened_boxes:
+                    Stack.append(key)
 
-    return len(opened_boxes) == len(boxes)
+    return len(opened_boxes) == n
