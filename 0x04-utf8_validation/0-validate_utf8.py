5651a02dcc94d576ff4358b9aba7314621df82b7
Lamba237 <simontanyi2004@gmail.com>
2024-10-31 23:54:37 +0100

    updates

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,34 +3,42 @@
 
 
 def validUTF8(data):
-    """This function checks if a list is a
-    valid UTF-8 encoding
+    """This function checks if a list is a valid UTF-8 encoding
 
     Args:
         data: receives a list of codes to be checked
 
     Return: True if UTF-8 else False
     """
+    def is_valid_byte(byte):
+        return byte & 0b11000000 == 0b10000000
 
     i = 0
-
     while i < len(data):
-        if data[i] < 0x80:
+        byte = data[i]
+        if byte & 0b10000000 == 0:
+            # 1-byte character
             i += 1
             continue
-
-        num_bytes = 0
-        if data[i] & 0xF0 == 0xF0:
-            num_bytes = 4
-        elif data[i] & 0xE0 == 0xE0:
-            num_bytes = 3
-        elif data[i] & 0xC0 == 0xC0:
+        elif byte & 0b11100000 == 0b11000000:
+            # 2-byte character
             num_bytes = 2
+        elif byte & 0b11110000 == 0b11100000:
+            # 3-byte character
+            num_bytes = 3
+        elif byte & 0b11111000 == 0b11110000:
+            # 4-byte character
+            num_bytes = 4
         else:
             return False
 
+        if i + num_bytes > len(data):
+            return False
+
         for j in range(1, num_bytes):
-            if i + j >= len(data) or (data[i+1] & 0xC0) != 0x80:
+            if not is_valid_byte(data[i + j]):
                 return False
-            i += num_bytes
+
+        i += num_bytes
+
     return True
