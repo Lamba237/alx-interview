b1cd8ae00d57277099f188d14bad7477f3a69f8f
Lamba237 <simontanyi2004@gmail.com>
2024-11-28 02:36:52 +0100

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed
+    to meet a given amount total.
+
+    Args:
+    coins: list of value of coins in my possession
+    total: amount of money needed to meet a given amount of total
+
+    Return: fewest number of coins needed
+    """
+    
+    if total <= 0:
+        return 0
+
+    # Initialize the dp array with a value greater than the maximum possible number of coins
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
